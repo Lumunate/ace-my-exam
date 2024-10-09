9227cffbf30c7423f222f93a9cc8426f416abcbb
Faizan Haq <fizoneechan@gmail.com>
2024-10-10 01:36:24 +0500

    added fonts and move stats to components

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import { AppContentWrapper } from "@/components/common/Global.style";
 import Hero from "@/components/Home/Hero/Hero";
-import Stats from "../../../public/Stats/Stats";
+import Stats from "../../components/Home/Stats/Stats";
 
 export default function Home() {
   return (
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-import { Poppins } from '@next/font/google'
-
-const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
-  display: 'swap',
-})
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>
+      <body>
         {children}
       </body>
     </html>
