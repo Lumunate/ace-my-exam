6820e2fdd19cc883ea816893bea687a869e947b6
Faizan Haq <fizoneechan@gmail.com>
2024-10-11 16:04:42 +0500

    .

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,7 @@
 import { AppContentWrapper } from "@/components/common/Global.style";
 import Hero from "@/components/Home/Hero/Hero";
 import Stats from "../../components/Home/Stats/Stats";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -9,6 +10,9 @@ export default function Home() {
 
       <AppContentWrapper>
         <Stats />
+
+        <Image src={"/Decorative/crosses.svg"} width={14} height={80} alt="crosses" style={{ float: "right" }} />
+
       </AppContentWrapper>
     </>
   );
--- src/components/common/Global.style.ts
+++ src/components/common/Global.style.ts
@@ -3,5 +3,6 @@ import { Box, styled } from "@mui/material";
 
 export const AppContentWrapper = styled(Box)({
   maxWidth: "1600px",
+  padding: "0 2rem",
   margin: "0 auto",
 });
