d1cb5262e2fb31e9c1d351e538bed78d331bf7e0
MEH-NAZZ <msmeh.naz07@gmail.com>
2024-10-13 17:56:22 +0500

    Finalise Testimonials Home (PR #8 Changes): Integrated testimonials with Swiper component and separated testimonials data into JSON

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,3 @@
-// import Image from 'next/image';
-
 import { AppContentWrapper } from '@/components/common/Global.style';
 import Hero from '@/features/home/hero/Hero';
 import Stats from '@/features/home/stats/Stats';
@@ -9,14 +7,9 @@ export default function Home() {
   return (
     <>
       <Hero />
-
       <AppContentWrapper>
         <Stats />
-
-        {/* <Image src={'/decorative/crosses.svg'} width={14} height={80} alt="crosses" style={{ float: 'right' }} /> */}
-
         <Testimonials />
-
       </AppContentWrapper>
     </>
   );
