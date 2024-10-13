55809f2f78adf69414a92c8c5cc687088638e973
Faizan Haq <fizoneechan@gmail.com>
2024-10-13 21:45:10 +0500

    fix build warnings and use isLoading from react query

--- src/components/form/Form.style.ts
+++ src/components/form/Form.style.ts
@@ -10,8 +10,8 @@ export const StyledTextField = styled(TextField)<{
     fontSize: inputfontsize,
     fontFamily: 'Jost, sans-serif',
   },
-  '& .MuiInputLabel-root': {
-    color: '#818181',
+  "& .MuiInputLabel-root": {
+    color: "#818181",
     fontWeight: 600,
     fontSize: labelfontsize,
     textTransform: 'capitalize',
@@ -20,17 +20,17 @@ export const StyledTextField = styled(TextField)<{
   '& .MuiFormHelperText-root.Mui-error ': {
     fontSize: '10px',
   },
-  '& .MuiInputLabel-root.Mui-focused': {
-    color: '#818181',
-    transform: 'scale(0.9)',
+  "& .MuiInputLabel-root.Mui-focused": {
+    color: "#818181",
+    transform: "scale(0.9)",
   },
-  '& .MuiInput-underline:before': {
-    borderBottom: '1px solid #818181',
+  "& .MuiInput-underline:before": {
+    borderBottom: "1px solid #818181",
   },
-  '& .MuiInput-underline:after': {
-    borderBottom: '1px solid black',
+  "& .MuiInput-underline:after": {
+    borderBottom: "1px solid black",
   },
-  '& .MuiInput-underline:hover:not(.Mui-disabled):before': {
-    borderBottom: '1px solid black',
+  "& .MuiInput-underline:hover:not(.Mui-disabled):before": {
+    borderBottom: "1px solid black",
   },
 }));
