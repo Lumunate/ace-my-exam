9c36afa1778e142b2472de81a9802692aff2bf8d
MEH-NAZZ <msmeh.naz07@gmail.com>
2024-10-15 18:10:46 +0500

    CSS improvements are needed in the hero, state, and navbar sections.

--- src/features/home/stats/Stats.style.ts
+++ src/features/home/stats/Stats.style.ts
@@ -2,6 +2,11 @@
 
 import { Box, styled, Typography, Card } from '@mui/material';
 
+const CommonStatTypography = styled(Typography)({
+  fontStyle: 'normal',
+  fontFamily: 'var(--font-lato)', 
+});
+
 export const StatsContentWrapper = styled(Box)({
   width: '100%',
   position: 'relative',
@@ -10,43 +15,41 @@ export const StatsContentWrapper = styled(Box)({
   '&::before': {
     content: '""',
     position: 'absolute',
-    bottom: '0',         
-    left: '0',       
-    width: '100%',   
-    height: '50%',   
-    backgroundColor: '#ebfdfe', 
+    bottom: '0',
+    left: '0',
+    width: '100%',
+    height: '50%',
+    backgroundColor: '#ebfdfe',
     borderRadius: '100px 100px 0 0',
-    zIndex: '-1',      
+    zIndex: '-1',
   },
 });
 
 export const StatsCardHeah = styled(Box)({
-  maxWidth: '1040px', 
-  margin: '0 auto'
+  maxWidth: '1040px',
+  margin: '0 auto',
 });
+
 export const StatsCard = styled(Card)({
   borderRadius: '20px',
   background: '#FFF',
-  boxShadow:' 0px 4px 39.5px 0px rgba(0, 0, 0, 0.10)',
+  boxShadow: '0px 4px 39.5px 0px rgba(0, 0, 0, 0.10)',
   padding: '56px 37px',
   textAlign: 'center',
-  cursor: 'pointer'
+  cursor: 'pointer',
 });
 
-export const StatCardHeading = styled(Typography)({
+export const StatCardHeading = styled(CommonStatTypography)({
   fontWeight: 700,
   fontSize: '16px',
   color: '#111826',
-  fontFamily: 'var(--font-lato)',
-  fontStyle: 'normal',
 });
 
-export const StatCardValue = styled(Typography)({
+export const StatCardValue = styled(CommonStatTypography)({
   fontWeight: 500,
   fontSize: '49px',
   color: '#0ABCAA',
-  fontFamily: 'var(--font-jost)',
-  fontStyle: 'normal',
+  fontFamily: 'var(--font-jost)', 
   textShadow: '0px 4px 4px rgba(0, 0, 0, 0.16)',
-  margin: '4px 0'
+  margin: '4px 0',
 });
