352b9e363c4cd978e81937cf9546fd5d6e34100a
MEH-NAZZ <msmeh.naz07@gmail.com>
2024-10-15 20:50:35 +0500

    build test 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,19 +4,19 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { SnackbarProvider } from '@/contexts/SnackbarContext';
 import './globals.css';
 
-import { Jost, Lato } from 'next/font/google';
+// import { Jost, Lato } from 'next/font/google';
 
-const jost = Jost({
-  subsets: ['latin'],
-  weight: ['100','200', '300', '400', '500', '600', '700', '800', '900'], 
-  variable: '--font-jost',
-});
+// const jost = Jost({
+//   subsets: ['latin'],
+//   weight: ['100','200', '300', '400', '500', '600', '700', '800', '900'], 
+//   variable: '--font-jost',
+// });
 
-const lato = Lato({
-  subsets: ['latin'],
-  weight: ['100', '300', '400','700','900'],
-  variable: '--font-lato',
-});
+// const lato = Lato({
+//   subsets: ['latin'],
+//   weight: ['100', '300', '400','700','900'],
+//   variable: '--font-lato',
+// });
 
 const queryClient = new QueryClient();
 
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${jost.variable} ${lato.variable}`}>
+    <html lang="en">
       <body>
         <QueryClientProvider client={queryClient}>
           <SnackbarProvider>{children}</SnackbarProvider>
