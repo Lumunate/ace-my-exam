83d970bf71c81e10cbdefc89944be39fdb315b40
Faizan Haq <fizoneechan@gmail.com>
2024-10-15 15:06:21 +0500

    wip: user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,28 +1,46 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, ManyToMany } from "typeorm";
+import { ValueTransformer } from "typeorm";
 
-@Entity('users')
+const transformer: Record<"date" | "bigint", ValueTransformer> = {
+  date: {
+    from: (date: string | null) => date && new Date(parseInt(date, 10)),
+    to: (date?: Date) => date?.valueOf().toString(),
+  },
+  bigint: {
+    from: (bigInt: string | null) => bigInt && parseInt(bigInt, 10),
+    to: (bigInt?: number) => bigInt?.toString(),
+  },
+};
+
+@Entity({ name: "users" })
 export default class User {
-  @PrimaryGeneratedColumn()
-    id!: number;
+  @PrimaryGeneratedColumn("uuid")
+  id!: string;
+
+  @Column({ type: "varchar", nullable: true })
+  name!: string | null;
+
+  @Column({ type: "varchar", nullable: true, unique: true })
+  email!: string | null;
+
+  @Column({ type: "varchar", nullable: true, transformer: transformer.date })
+  emailVerified!: string | null;
+
+  @Column({ type: "varchar", nullable: true })
+  image!: string | null;
+
+  @Column({ type: "varchar", nullable: true })
+  role!: string | null;
+
+  @OneToMany(() => SessionEntity, (session) => session.userId)
+  sessions!: SessionEntity[];
+
+  @OneToMany(() => AccountEntity, (account) => account.userId)
+  accounts!: AccountEntity[];
+
+  @ManyToMany(() => VerificationTokenEntity, (token) => token.id_token)
+  accounts!: AccountEntity[];
 
   @Column()
-    name!: string;
-
-  @Column({ unique: true })
-    email!: string;
-
-  @Column()
-    password!: string;
-
-  @Column({ default: false })
-    emailVerified!: boolean;
-
-  @Column({ nullable: true })
-    verificationToken!: string;
-
-  @Column({ default: null })
-    resetPasswordToken?: string | null;
-
-  @Column({ default: null })
-    resetPasswordExpires?: Date | null;
+  password!: string;
 }
