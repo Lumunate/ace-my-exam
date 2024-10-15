0f47609b552755bbdd0944b56e4b458d2b7177a0
MEH-NAZZ <msmeh.naz07@gmail.com>
2024-10-15 19:20:26 +0500

    build issue fix

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,5 @@
 import { AppContentWrapper } from '@/components/common/Global.style';
-import About from '@/features/home/about/About';
+// import About from '@/features/home/about/About';
 import Hero from '@/features/home/hero/Hero';
 import Stats from '@/features/home/stats/Stats';
 import Testimonials from '@/features/home/testimonials/Testimonials';
@@ -10,7 +10,7 @@ export default function Home() {
       <Hero />
       <Stats />
       <AppContentWrapper>
-        <About />
+        {/* <About /> */}
         <Testimonials />
       </AppContentWrapper>
     </>
