b18c269d270c6bb12da4c14641700f6b35f48e34
Faizan Haq <fizoneechan@gmail.com>
2024-11-03 16:00:53 +0500

    new admin ui

---
+++ src/app/admin/account/layout.tsx
@@ -0,0 +1,16 @@
+import AccountHeader from '@/features/admin/account/AccountHeader';
+
+const AdminAccountLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <AccountHeader />
+      {children}
+    </>
+  );
+};
+
+export default AdminAccountLayout;
