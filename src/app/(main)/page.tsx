690adb3e9ca04c5da64a239dd510ee08071432a4
Faizan Haq <fizoneechan@gmail.com>
2024-11-09 23:32:26 +0500

    page scroll animation

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import { Box } from '@mui/material';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect, useRef } from 'react';
+
 import { AppContentWrapper } from '@/components/common/Global.style';
 import About from '@/features/home/about/About';
 import Hero from '@/features/home/hero/Hero';
@@ -10,23 +15,83 @@ import Stats from '@/features/home/stats/Stats';
 import Support from '@/features/home/support/Support';
 import Testimonials from '@/features/home/testimonials/Testimonials';
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Home() {
+  const contentWrapperRef = useRef(null);
+
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.scrolling-section',
+        start: 'top  center',
+        end: '+=300%',
+        scrub: true,
+        pin: true,
+        pinSpacing: false,
+      },
+    });
+
+    tl.to(
+      '.scrolling-section',
+      {
+        opacity: 0,
+        duration: 0.5,
+      },
+      '<'
+    );
+    // .to(
+    //   contentWrapperRef.current,
+    //   {
+    //     scrollTrigger: {
+    //       trigger: ".scrolling-section",
+    //       start: "top center", 
+    //       end: "bottom bottom",
+
+    //       onEnter: () => {
+    //         window.scrollTo({
+    //           top: window.scrollY + window.innerHeight * 0.95,
+    //           behavior: "smooth"
+    //         });
+    //       },
+
+    //     },
+    //   },
+    //   ">"
+    // )
+
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
+    };
+  }, []);
+
   return (
-    <>
+    <Box ref={contentWrapperRef}>
       <Hero />
       <Stats />
       <AppContentWrapper>
         <About />
       </AppContentWrapper>
-      <Process />
-      <Institutes />
-      <AppContentWrapper>
-        <Resources />
-      </AppContentWrapper>
-      <Support />
-      <AppContentWrapper>
-        <Testimonials />
-      </AppContentWrapper>
-    </>
+
+      <Box className="scrolling-section">
+        <Process />
+      </Box>
+      <Box
+        sx={{
+          backgroundColor: '#fffbfb',
+          zIndex: 20,
+          position: 'relative',
+        }}
+      >
+        <Institutes />
+        <AppContentWrapper>
+          <Resources />
+        </AppContentWrapper>
+        <Support />
+        <AppContentWrapper>
+          <Testimonials />
+        </AppContentWrapper>
+      </Box>
+    </Box>
   );
 }
