db4a6151c32395379e3449c364e5ee41f01e0665
Faizan Haq <fizoneechan@gmail.com>
2024-11-12 19:30:39 +0500

    added page scroll animation to below resources

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,16 @@
-'use client';
-import { Box } from '@mui/material';
+"use client";
+import { Box } from "@mui/material";
 
-import ScrollingFadeOut from '@/components/animations/ScrollingFadeOut';
-import { AppContentWrapper } from '@/components/common/Global.style';
-import About from '@/features/home/about/About';
-import Hero from '@/features/home/hero/Hero';
-import Institutes from '@/features/home/Institutes/Institutes';
-import Process from '@/features/home/process/Process';
-import Resources from '@/features/home/resources/Resources';
-import Stats from '@/features/home/stats/Stats';
-import Support from '@/features/home/support/Support';
-import Testimonials from '@/features/home/testimonials/Testimonials';
+import ScrollingFadeOut from "@/components/animations/ScrollingFadeOut";
+import { AppContentWrapper } from "@/components/common/Global.style";
+import About from "@/features/home/about/About";
+import Hero from "@/features/home/hero/Hero";
+import Institutes from "@/features/home/Institutes/Institutes";
+import Process from "@/features/home/process/Process";
+import Resources from "@/features/home/resources/Resources";
+import Stats from "@/features/home/stats/Stats";
+import Support from "@/features/home/support/Support";
+import Testimonials from "@/features/home/testimonials/Testimonials";
 
 export default function Home() {
   return (
@@ -26,19 +26,30 @@ export default function Home() {
       </ScrollingFadeOut>
       <Box
         sx={{
-          backgroundColor: '#fffbfb',
+          backgroundColor: "#fffbfb",
           zIndex: 20,
-          position: 'relative',
+          position: "relative",
         }}
       >
         <Institutes />
-        <AppContentWrapper>
-          <Resources />
-        </AppContentWrapper>
-        <Support />
-        <AppContentWrapper>
-          <Testimonials />
-        </AppContentWrapper>
+        <ScrollingFadeOut>
+          <AppContentWrapper>
+            <Resources />
+          </AppContentWrapper>
+        </ScrollingFadeOut>
+
+        <Box
+          sx={{
+            backgroundColor: "#fffbfb",
+            zIndex: 20,
+            position: "relative",
+          }}
+        >
+          <Support />
+          <AppContentWrapper>
+            <Testimonials />
+          </AppContentWrapper>
+        </Box>
       </Box>
     </>
   );
