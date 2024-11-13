312cdcebf26d66d48816b0008539c8c6b6c4cf36
Faizan Haq <fizoneechan@gmail.com>
2024-11-14 01:45:19 +0500

    Revert "add subject FK to past paper"
    This reverts commit 008ff19f36308bb57651c6be1e22ea272d0b8ef1.

--- src/entities/past-paper.ts
+++ src/entities/past-paper.ts
@@ -1,24 +1,21 @@
-import { Entity, Column, OneToMany, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, Column, OneToMany } from 'typeorm';
 
 import { BaseEntity } from './base-entity';
 import { PastPaperResource } from './past-paper-resource';
-import { Subject } from './subject';
-@Entity("past_paper")
+@Entity('past_paper')
 export class PastPaper extends BaseEntity {
   @Column()
-  title: string;
+    title: string;
 
   @Column()
-  year: number;
+    year: number;
 
-  @OneToMany("PastPaperResource", (pastPaperResource: PastPaperResource) => pastPaperResource.pastPaper, {
-    cascade: true,
-  })
-  resources?: PastPaperResource[];
-
-  @ManyToOne(() => Subject, (subject) => subject.contents, {
-    onDelete: "SET NULL",
-  })
-  @JoinColumn({ name: "subject_id" })
-  subject?: Subject;
+  @OneToMany(
+    'PastPaperResource',
+    (pastPaperResource: PastPaperResource) => pastPaperResource.pastPaper,
+    {
+      cascade: true
+    }
+  )
+    resources?: PastPaperResource[];
 }
