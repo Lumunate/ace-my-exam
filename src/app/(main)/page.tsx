aef7db2e181660afd2ed1655455b086189d42276
anasbinarif <58784109+anasbinarif@users.noreply.github.com>
2024-11-22 23:35:47 +0100

    deploy bug fix related to imports 2

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,17 +1,16 @@
 'use client';
 import { Box } from '@mui/material';
 
-import Process from '@/features/home/process/Process';
-import Resources from '@/features/home/resources/Resources';
-import Stats from '@/features/home/stats/Stats';
-import Support from '@/features/home/support/Support';
-import Testimonials from '@/features/home/testimonials/Testimonials';
-
 import ScrollingFadeOut from '../../components/animations/ScrollingFadeOut';
 import { AppContentWrapper } from '../../components/common/Global.style';
 import About from '../../features/home/about/About';
 import Hero from '../../features/home/hero/Hero';
 import Institutes from '../../features/home/Institutes/Institutes';
+import Process from '../../features/home/process/Process';
+import Resources from '../../features/home/resources/Resources';
+import Stats from '../../features/home/stats/Stats';
+import Support from '../../features/home/support/Support';
+import Testimonials from '../../features/home/testimonials/Testimonials';
 
 export default function Home() {
   return (
