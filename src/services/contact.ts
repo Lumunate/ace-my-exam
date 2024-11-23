7501c95bbda945e1a024cbaa53ecd8302d6edb61
Faizan Haq <fizoneechan@gmail.com>
2024-11-23 10:08:32 +0500

    contact acknowledgement

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -1,14 +1,32 @@
-import * as ContactRepository from '../repositories/contact';
-import { IContact } from '../types/contact';
+import ContactAcknowledgmentTemplate, { ContactAcknowledgmentProps } from "emails/contact-acknowledgement";
+import * as ContactRepository from "../repositories/contact";
+import { IContact } from "../types/contact";
+import { render } from "@react-email/render";
+import { EmailError, sendEmail } from "utils/send-email";
 
 export async function createContact(data: IContact) {
   const contact = await ContactRepository.createContact(data);
 
-  sendAcknowledgementEmail(data);
+  sendAcknowledgementEmail(data.email, data);
 
   return contact;
 }
 
-async function sendAcknowledgementEmail(_contact: IContact) {
-  // TODO: Send an email to the user acknowledging the receipt of the message
+async function sendAcknowledgementEmail(email: string, data: ContactAcknowledgmentProps): Promise<string> {
+  try {
+    const html = await render(ContactAcknowledgmentTemplate(data));
+
+    return await sendEmail(html, {
+      to: email,
+      subject: "Thank you for contacting Ace My Exams",
+      from: "fizoneechan@gmail.com",
+      fromName: "Ace My Exams",
+      replyTo: "acemyexams@gmail.com",
+    });
+  } catch (error) {
+    if (error instanceof EmailError) {
+      throw error;
+    }
+    throw new EmailError("Failed to prepare verification email", error);
+  }
 }
