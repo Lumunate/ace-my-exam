1a3d6de086f40c7dff3a6c6324cea392e81993ca
Faizan Haq <fizoneechan@gmail.com>
2024-11-23 07:49:28 +0500

    build errors 2

--- src/features/resources/resources-tables/PastPapersTable.tsx
+++ src/features/resources/resources-tables/PastPapersTable.tsx
@@ -5,8 +5,6 @@ import { PastPaper } from '@prisma/client';
 import Image from 'next/image';
 import React from 'react';
 
-import { StyledPagination } from '../../../components/pagination/Pagination.style';
-
 import {
   ChapterHeading,
   CollapseContainer,
@@ -17,6 +15,7 @@ import {
   ExpandIconHead
 } from './ResourceTables.style';
 import { PaginationHead, ResourcesPara, ResourcesSubHeading } from '../../../app/(main)/resources/Resources.style';
+import { StyledPagination } from '../../../components/pagination/Pagination.style';
 
 const PastPapersTable: React.FC<{ data: PastPaper[]; isLoading: boolean }> = ({ data }) => {
 
