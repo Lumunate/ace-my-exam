4133a4e17b7ac014a35a762fd08a1ddfff761164
Faizan Haq <fizoneechan@gmail.com>
2024-11-24 03:54:12 +0500

    client reciept email

--- src/services/feedback.ts
+++ src/services/feedback.ts
@@ -1,16 +1,18 @@
-import { render } from '@react-email/render';
+import { render } from "@react-email/render";
 
-import FeedbackAcknowledgmentTemplate, { FeedbackAcknowledgmentProps } from 'emails/feedback-acknowledgement';
-import { EmailError, sendEmail } from 'utils/send-email';
+import FeedbackAcknowledgmentTemplate, { FeedbackAcknowledgmentProps } from "emails/feedback-acknowledgement";
+import { EmailError, sendEmail } from "utils/send-email";
 
-import * as FeedbackRepository from '../repositories/feedback';
-import { IFeedback } from '../types/feedback';
+import * as FeedbackRepository from "../repositories/feedback";
+import { IFeedback } from "../types/feedback";
+import NewFeedbackNotificationTemplate, { NewFeedbackNotificationProps } from "emails/feedback-form-submission";
 
 export async function createFeedback(data: IFeedback) {
   const feedback = await FeedbackRepository.createFeedback(data);
 
   // sendAcknowledgementEmail(data., data);
-
+  sendClientRecieveEmail(feedback);
+  
   return feedback;
 }
 
@@ -29,6 +31,25 @@ export async function _sendAcknowledgementEmail(email: string, data: FeedbackAck
     if (error instanceof EmailError) {
       throw error;
     }
-    throw new EmailError('Failed to prepare verification email', error);
+    throw new EmailError("Failed to prepare verification email", error);
+  }
+}
+
+export async function sendClientRecieveEmail(data: NewFeedbackNotificationProps): Promise<string> {
+  try {
+    const html = await render(NewFeedbackNotificationTemplate(data));
+
+    return await sendEmail(html, {
+      to: "asma@acemyexam.co.uk",
+      subject: "New Feedback Form Submission",
+      from: "fizoneechan@gmail.com",
+      fromName: "Ace My Exams",
+      replyTo: "asma@acemyexam.co.uk",
+    });
+  } catch (error) {
+    if (error instanceof EmailError) {
+      throw error;
+    }
+    throw new EmailError("Failed to prepare client recieve email", error);
   }
 }
