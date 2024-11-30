2882130d722d7a781638d6cd15aca3a5e2a12506
anasbinarif <58784109+anasbinarif@users.noreply.github.com>
2024-11-30 19:32:05 +0100

    added log for middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,8 @@ import {getToken} from 'next-auth/jwt';
 export default async function middleware(req) {
   const token = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
 
+  console.log('token debug 101', token);
+
   if (token && token.role !== 'ADMIN') {
     return NextResponse.redirect(new URL('/', req.url));
   }
