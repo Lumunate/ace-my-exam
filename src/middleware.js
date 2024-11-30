836904f77fadd421556d08f12310c0935610bc75
anasbinarif <58784109+anasbinarif@users.noreply.github.com>
2024-11-30 20:02:01 +0100

    resolved redirect issue

--- src/middleware.js
+++ src/middleware.js
@@ -4,9 +4,7 @@ import {getToken} from 'next-auth/jwt';
 export default async function middleware(req) {
   const token = await getToken({req, secret: process.env.NEXTAUTH_SECRET});
 
-  console.log('token debug 101', token);
-
-  if (token && token.role !== 'ADMIN') {
+  if (!token || token.role !== 'ADMIN') {
     return NextResponse.redirect(new URL('/', req.url));
   }
 
