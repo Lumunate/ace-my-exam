40d78c0212b1875429df629dd84db55c5d657675
Faizan Haq <fizoneechan@gmail.com>
2024-12-01 07:59:19 +0500

    fix rendering logic

--- src/features/admin/resources/SelectionForm.tsx
+++ src/features/admin/resources/SelectionForm.tsx
@@ -39,7 +39,8 @@ const SelectionForm = () => {
             subject={selectedSubjectSubtype}
             resourceType={selectedResourceType as ResourceType}
           />
-          {selectedSubtopic || selectedPastPaper && (
+
+          {(!!selectedSubtopic || !!selectedPastPaper) && (
             <>
               <AdminSectionInnerSpacer />
 
