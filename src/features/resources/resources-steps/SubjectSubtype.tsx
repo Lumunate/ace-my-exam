7251deeeb4c48de1f29d03d130e14bebc37a5b6e
Faizan Haq <fizoneechan@gmail.com>
2024-12-08 12:09:34 +0500

    feat: add subject tag icons and improve subject selection UI in SubjectSubtype component

--- src/features/resources/resources-steps/SubjectSubtype.tsx
+++ src/features/resources/resources-steps/SubjectSubtype.tsx
@@ -1,10 +1,11 @@
 'use client';
 
 import Grid from '@mui/material/Grid2';
+import Image from 'next/image';
 import React, { useEffect } from 'react';
 
-import {  ResourcesCardSmall } from '../ResourcesSteps.style';
-import {  StepsLoader } from './StepsLoader';
+import { ResourcesCardSmall } from '../ResourcesSteps.style';
+import { StepsLoader } from './StepsLoader';
 import { ResourcesCardTypography } from '../../../app/(main)/resources/Resources.style';
 import { IStepOption } from '../../../contexts/MultiStepperContext';
 import { useGetSubjects } from '../../../hooks/resources/useReferenceData';
@@ -51,72 +52,113 @@ const Year1Year2Subjects = ({
   selectedSubjectSubtype: IStepOption;
   selectOption: (stepName: string, option: IStepOption) => void;
 }) => {
-  const year1Subjects = subjectOptions.filter((s) => s.tags?.includes('YEAR_1')).map((s) => ({
-    ...s, tags: s.tags?.filter((s) => {
-      return s !== 'YEAR_1';
-    })
-  }));
-  const year2Subjects = subjectOptions.filter((s) => s.tags?.includes('YEAR_2')).map((s) => ({
-    ...s, tags: s.tags?.filter((s) => {
-      return s !== 'YEAR_2';
-    })
-  }));
+  const year1Subjects = subjectOptions
+    .filter((s) => s.tags?.includes('YEAR_1'))
+    .map((s) => ({
+      ...s,
+      tags: s.tags?.filter((s) => {
+        return s !== 'YEAR_1';
+      }),
+    }));
+  const year2Subjects = subjectOptions
+    .filter((s) => s.tags?.includes('YEAR_2'))
+    .map((s) => ({
+      ...s,
+      tags: s.tags?.filter((s) => {
+        return s !== 'YEAR_2';
+      }),
+    }));
+
+  const getTagIcon = (tags: string[]) => {
+    if (tags.some((tag) => tag.includes('Pure'))) return '/resources/math.svg';
+    if (tags.some((tag) => tag.includes('Statistics'))) return '/resources/Physics.svg';
+    if (tags.some((tag) => tag.includes('Mechanics'))) return '/resources/chemistry.svg';
+
+    return '/resources/Science.svg';
+  };
 
   return (
-    <Grid
-      container
-      rowSpacing={'17px'} columnSpacing={'24px'}
-      sx={{ mb: '40px' }}
-      columns={12}
-    >
-      {
-        year1Subjects.map((subject, index) => (
+    <Grid container rowSpacing={'17px'} columnSpacing={'24px'} sx={{ mb: '40px' }} columns={12}>
+      <Grid
+        container
+        rowSpacing={'17px'}
+        columnSpacing={'24px'}
+        sx={{ mb: '40px' }}
+        columns={6}
+        size={{
+          xs: 12,
+          lg: 6,
+        }}
+      >
+        {year1Subjects.map((subject) => (
           <Grid
+            key={subject.id}
             size={{
               xs: 12,
               lg: 6,
             }}
-            gridArea={`0/${index}`}
-            key={subject.id}
           >
             <ResourcesCardSmall
-              onClick={() => selectOption('subjectSubtype', { name: `AS ${subject.tags.join(' - ')}`, value: subject.id?.toString(), icon: '' })}
+              onClick={() =>
+                selectOption('subjectSubtype', {
+                  name: `AS ${subject.tags.join(' - ')}`,
+                  value: subject.id?.toString(),
+                  icon: '',
+                })
+              }
               sx={{
                 outline: selectedSubjectSubtype?.value === subject.id?.toString() ? '1px solid #DA9694' : 'unset',
               }}
             >
-              <ResourcesCardTypography variant="body1" sx={{ ml: '4px', }}>
+              <Image src={getTagIcon(subject.tags)} alt={subject.tags?.join(' - ')} width={15} height={15} />
+              <ResourcesCardTypography variant="body1" sx={{ ml: '4px' }}>
                 AS {subject.tags.join(' - ')}
               </ResourcesCardTypography>
             </ResourcesCardSmall>
           </Grid>
-        ))
-      }
+        ))}
+      </Grid>
 
-      {
-        year2Subjects.map((subject, index) => (
+      <Grid
+        container
+        rowSpacing={'17px'}
+        columnSpacing={'24px'}
+        sx={{ mb: '40px' }}
+        columns={6}
+        size={{
+          xs: 12,
+          lg: 6,
+        }}
+      >
+        {year2Subjects.map((subject) => (
           <Grid
+            key={subject.id}
             size={{
               xs: 12,
               lg: 6,
             }}
-            gridArea={`1/${index}`}
-            key={subject.id}
           >
             <ResourcesCardSmall
-              onClick={() => selectOption('subjectSubtype', { name: `A Level ${subject.tags.join(' - ')}`, value: subject.id?.toString(), icon: '' })}
+              onClick={() =>
+                selectOption('subjectSubtype', {
+                  name: `A Level ${subject.tags.join(' - ')}`,
+                  value: subject.id?.toString(),
+                  icon: '',
+                })
+              }
               sx={{
                 outline: selectedSubjectSubtype?.value === subject.id?.toString() ? '1px solid #DA9694' : 'unset',
               }}
             >
-              <ResourcesCardTypography variant="body1" sx={{ ml: '4px', }}>
+              <Image src={getTagIcon(subject.tags)} alt={subject.tags?.join(' - ')} width={15} height={15} />
+              <ResourcesCardTypography variant="body1" sx={{ ml: '4px' }}>
                 A Level {subject.tags.join(' - ')}
               </ResourcesCardTypography>
             </ResourcesCardSmall>
           </Grid>
-        ))
-      }
-    </Grid >
+        ))}
+      </Grid>
+    </Grid>
   );
 };
 
@@ -133,13 +175,17 @@ const SimpleSubjects = ({
   selectedSubjectSubtype: IStepOption;
   selectOption: (stepName: string, option: IStepOption) => void;
 }) => {
+  const getTagIcon = (tags: string[]) => {
+    if (tags.some((tag) => tag.includes('Core'))) return '/resources/core-icon.svg';
+    if (tags.some((tag) => tag.includes('Extended'))) return '/resources/extended-icon.svg';
+    if (tags.some((tag) => tag.includes('Higher'))) return '/resources/core-icon.svg';
+    if (tags.some((tag) => tag.includes('Foundation'))) return '/resources/extended-icon.svg';
+
+    return '/resources/core-icon.svg';
+  };
+
   return (
-    <Grid
-      container
-      spacing={'20px'}
-      sx={{ mb: '40px' }}
-      columns={12}
-    >
+    <Grid container spacing={'20px'} sx={{ mb: '40px' }} columns={12}>
       {subjectOptions.map((subject) => (
         <Grid
           size={{
@@ -149,12 +195,15 @@ const SimpleSubjects = ({
           key={subject.id}
         >
           <ResourcesCardSmall
-            onClick={() => selectOption('subjectSubtype', { name: subject.tags?.join(' - '), value: subject.id?.toString(), icon: '' })}
+            onClick={() =>
+              selectOption('subjectSubtype', { name: subject.tags?.join(' - '), value: subject.id?.toString(), icon: '' })
+            }
             sx={{
               outline: selectedSubjectSubtype?.value === subject.id?.toString() ? '1px solid #DA9694' : 'unset',
             }}
           >
-            <ResourcesCardTypography variant="body1" sx={{ ml: '4px', }}>
+            <Image src={getTagIcon(subject.tags)} alt={subject.tags?.join(' - ')} width={15} height={15} />
+            <ResourcesCardTypography variant="body1" sx={{ ml: '4px' }}>
               {subject.tags?.join(' - ')}
             </ResourcesCardTypography>
           </ResourcesCardSmall>
